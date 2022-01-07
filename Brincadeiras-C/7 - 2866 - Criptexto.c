4e4763aac522fed6c8a685f13d46068937a44025
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-01-07 19:39:50 -0300

    Update 7 - 2866 - Criptexto.c

--- Brincadeiras-C/7 - 2866 - Criptexto.c
+++ Brincadeiras-C/7 - 2866 - Criptexto.c
@@ -1,26 +1,22 @@
 /*
-César é um detetive que investiga uma série de roubos que acontecem em sua cidade. 
+Cï¿½sar ï¿½ um detetive que investiga uma sï¿½rie de roubos que acontecem em sua cidade. 
 Em todo lugar que um crime acontece, a pessoa que cometeu tal crime deixa uma mensagem escrita, 
-formada por letras maiúsculas e minúsculas. César conseguiu achar 
-um padrão nestas mensagens e agora extrai um texto oculto em cada mensagem e pede a sua ajuda para tentar 
-descobrir quem está cometendo tais crimes.
+formada por letras maiï¿½sculas e minï¿½sculas. Cï¿½sar conseguiu achar 
+um padrï¿½o nestas mensagens e agora extrai um texto oculto em cada mensagem e pede a sua ajuda para tentar 
+descobrir quem estï¿½ cometendo tais crimes.
 
 Entrada
-A entrada é composta por vários casos de teste. A primeira linha contém um número inteiro C (2 <= C <= 99) 
-relativo ao número de casos de teste. Nas C linhas seguintes, haverá mensagens codificadas, 
-todas com um mesmo padrão em relação ao exemplo abaixo.
+A entrada ï¿½ composta por vï¿½rios casos de teste. A primeira linha contï¿½m um nï¿½mero inteiro C (2 <= C <= 99) 
+relativo ao nï¿½mero de casos de teste. Nas C linhas seguintes, haverï¿½ mensagens codificadas, 
+todas com um mesmo padrï¿½o em relaï¿½ï¿½o ao exemplo abaixo.
 
-Saída
-Para cada caso de teste de entrada do seu programa, você deve imprimir o texto extraído da mensagem original.
+Saï¿½da
+Para cada caso de teste de entrada do seu programa, vocï¿½ deve imprimir o texto extraï¿½do da mensagem original.
 
 3
 NoTApasCAL                 sapo
 atEQUEatabELATERMINE       batata
 zoEIrrRRRRa                arroz
-
-
-
-
 */
 
 #include<stdio.h>
